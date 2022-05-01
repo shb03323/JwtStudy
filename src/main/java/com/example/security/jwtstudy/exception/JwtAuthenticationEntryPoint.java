305239eaa916effadff48de4ae31854d01ac16eb
jeonghoon <shb03323@naver.com>
2022-05-01 21:22:16 +0900

    [ADD] Handler

---
+++ src/main/java/com/example/security/jwtstudy/exception/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,20 @@
+package com.example.security.jwtstudy.exception;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+// 인증 실패 시
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+}
